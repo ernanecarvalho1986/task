d1c61b47aa9977bf2674b949de4feb1b9814051d
ernane <ernanecarvalho1986@gmail.com>
2026-05-12 08:18:31 -0300

    corrige app config

--- core/settings.py
+++ core/settings.py
@@ -24,7 +24,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'tasks',
+    'tasks.apps.TasksConfig',
     'django_apscheduler',
 ]
 
